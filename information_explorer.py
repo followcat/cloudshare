20d5a451c3b96917144508ca1ad464708d2d23ee
followcat <followcat@163.com>
2015-09-17 18:06:12 +0800

    Get name by re from markdown and show in listdata.

--- information_explorer.py
+++ information_explorer.py
@@ -4,6 +4,15 @@ import os.path
 import yaml
 
 
+def getNameFromString(stream):
+    name = ""
+    re_words = re.search(ur"\u59d3[\xa0 ]*\u540d[\xa0 :\uff1a]*"
+                         "(?P<name>[\S]*)\W", stream.decode('utf8'))
+    if re_words is not None:
+        name = re_words.group('name')
+    return name
+
+
 def getPhoneNumFromString(stream):
     regex = re.compile(r'1\d{10}', re.IGNORECASE)
     phonenums = re.findall(regex, stream)
@@ -27,6 +36,7 @@ def catch(path, convertname, output):
     with open(os.path.join(path, convertname.md), 'r') as f:
         stream = f.read()
         info_dict = {
+            "name": getNameFromString(stream),
             "mail": getMailAddFromString(stream),
             "phone": getPhoneNumFromString(stream)
             }

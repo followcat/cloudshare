60beced647efd693ca2fd63ca3d42e71a5a7e7df
followcat <followcat@163.com>
2015-09-18 12:26:40 +0800

    Add some doctests in information_explorer.

--- information_explorer.py
+++ information_explorer.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import re
 import os.path
 
@@ -5,6 +6,19 @@ import yaml
 
 
 def getNameFromString(stream):
+    """
+        >>> import information_explorer
+        >>> information_explorer.getNameFromString('姓名:followcat ')
+        u'followcat'
+        >>> information_explorer.getNameFromString('姓    名:followcat ')
+        u'followcat'
+        >>> information_explorer.getNameFromString('姓名:    followcat ')
+        u'followcat'
+        >>> information_explorer.getNameFromString('姓    名:    followcat ')
+        u'followcat'
+        >>> information_explorer.getNameFromString('  姓    名:    followcat ')
+        u'followcat'
+    """
     name = ""
     re_words = re.search(ur"\u59d3[\xa0 ]*\u540d[\xa0 :\uff1a]*"
                          "(?P<name>[\S]*)\W", stream.decode('utf8'))
@@ -14,12 +28,22 @@ def getNameFromString(stream):
 
 
 def getPhoneNumFromString(stream):
+    """
+        >>> import information_explorer
+        >>> information_explorer.getPhoneNumFromString('phone: 13123456789')
+        ['13123456789']
+    """
     regex = re.compile(r'1\d{10}', re.IGNORECASE)
     phonenums = re.findall(regex, stream)
     return phonenums
 
 
 def getMailAddFromString(stream):
+    """
+        >>> import information_explorer
+        >>> information_explorer.getMailAddFromString('Mail: followcat@gmail.com')
+        ['followcat@gmail.com']
+    """
     regex = re.compile(r"\b[a-zA-Z0-9._\\%+-]+"
                        r"@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b",
                        re.IGNORECASE)

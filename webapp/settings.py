d772b7f82f0be2beb4bc14ec00241f26fe45d0d6
followcat <followcat@163.com>
2015-10-20 18:11:48 +0800

    Add backup file, and called in post-receive.

--- webapp/settings.py
+++ webapp/settings.py
@@ -1,8 +1,14 @@
+import os.path
+
 import repointerface.gitinterface
 
 
 UPLOAD_TEMP = '/tmp'
 SECRET_KEY = 'SET T0 4NY SECRET KEY L1KE RAND0M H4SH'
 REPO_DB_NAME = 'repo'
+
+USER_HOME = os.path.expanduser("~")
+BACKUP_FOLDER = 'repodb_backup'
+BACKUP_DIR = os.path.join(USER_HOME, BACKUP_FOLDER)
 
 REPO_DB = repointerface.gitinterface.GitInterface(REPO_DB_NAME)

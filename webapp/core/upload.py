73f215d25b7835a468fbfd786647029d9cba1e5d
followcat <followcat@163.com>
2015-10-22 11:14:17 +0800

    Add class UploadObject and it's doctests.

---
+++ webapp/core/upload.py
@@ -0,0 +1,106 @@
+import os
+import codecs
+
+import yaml
+import pypandoc
+
+import core.exception
+import core.outputstorage
+import core.converterutils
+
+
+class UploadObject(object):
+
+    def __init__(self, filename, fileobject, path):
+        """
+            >>> import os
+            >>> import shutil
+            >>> import os.path
+            >>> import webapp.core.upload
+            >>> root = "core/test"
+            >>> name = "cv_1.doc"
+            >>> obj = open(os.path.join(root, name))
+            >>> test_path = "webapp/core/test_output"
+            >>> os.makedirs(test_path)
+            >>> up = webapp.core.upload.UploadObject(name, obj, test_path)
+            >>> up.result
+            True
+            >>> up = webapp.core.upload.UploadObject(name, obj, test_path)
+            >>> up.result
+            False
+            >>> up.information
+            'Exists File'
+            >>> up.remove()
+            >>> shutil.rmtree(test_path)
+        """
+        self.result = False
+        self.tmp_path = path
+        self.convertname = core.outputstorage.ConvertName(
+            filename.encode('utf-8'))
+        core.outputstorage.save_stream(self.tmp_path, self.convertname,
+                                       fileobject.read())
+        self.storage = core.converterutils.FileProcesser(self.tmp_path,
+                                                         self.convertname,
+                                                         self.tmp_path)
+        try:
+            self.result = self.storage.convert()
+            if self.result is False:
+                self.information = 'Can not Convert'
+            else:
+                self.information = 'Sucess'
+        except core.exception.DuplicateException:
+            self.information = 'Exists File'
+
+    def preview_yaml(self):
+        """
+            >>> import os
+            >>> import shutil
+            >>> import os.path
+            >>> import webapp.core.upload
+            >>> root = "core/test"
+            >>> name = "cv_1.doc"
+            >>> obj = open(os.path.join(root, name))
+            >>> test_path = "webapp/core/test_output"
+            >>> os.makedirs(test_path)
+            >>> up = webapp.core.upload.UploadObject(name, obj, test_path)
+            >>> yaml_data = up.preview_yaml()
+            >>> yaml_data['filename']
+            u'cv_1'
+            >>> up.remove()
+            >>> shutil.rmtree(test_path)
+        """
+        filename = os.path.join(self.storage.yaml_path, self.storage.name.yaml)
+        with open(filename, 'r') as yf:
+            stream = yf.read()
+        yaml_data = yaml.load(stream)
+        return yaml_data
+
+    def preview_markdown(self):
+        filename = os.path.join(self.storage.markdown_path,
+                                self.storage.name.md)
+        with codecs.open(filename, 'r', encoding='utf-8') as file:
+            data = file.read()
+        output = pypandoc.convert(data, 'html', format='markdown')
+        return output
+
+    def remove(self):
+        """
+            >>> import os
+            >>> import shutil
+            >>> import os.path
+            >>> import webapp.core.upload
+            >>> root = "core/test"
+            >>> name = "cv_1.doc"
+            >>> obj = open(os.path.join(root, name))
+            >>> test_path = "webapp/core/test_output"
+            >>> os.makedirs(test_path)
+            >>> up = webapp.core.upload.UploadObject(name, obj, test_path)
+            >>> fn = os.path.join(up.storage.markdown_path, up.storage.name.md)
+            >>> os.path.exists(fn)
+            True
+            >>> up.remove()
+            >>> os.path.exists(fn)
+            False
+            >>> shutil.rmtree(test_path)
+        """
+        self.storage.deleteconvert()

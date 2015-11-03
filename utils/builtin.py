8623443df60afd1f43c14d7c4eecd999bcbc2f1c
followcat <followcat@163.com>
2015-11-03 10:44:04 +0800

    Move convert_folder from converterutils to utils.builtin. Modify README.md

--- utils/builtin.py
+++ utils/builtin.py
@@ -1,7 +1,23 @@
+import os
 import hashlib
+
+import core.exception
+import core.converterutils
 
 
 def md5(text):
     m = hashlib.md5()
     m.update(text)
     return unicode(m.hexdigest())
+
+
+def convert_folder(path, repo, temp_output):
+    if not os.path.exists(temp_output):
+        os.makedirs(temp_output)
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            processfile = core.converterutils.FileProcesser(root, name, temp_output)
+            try:
+                processfile.storage(repo)
+            except core.exception.DuplicateException as error:
+                continue

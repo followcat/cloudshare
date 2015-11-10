dd88c0341bad4c98a5239def43a8523bffd792c4
followcat <followcat@163.com>
2015-11-10 10:38:08 +0800

    Move get name from 51job and filename code to function.

--- tools/batching.py
+++ tools/batching.py
@@ -16,11 +16,26 @@ def move_file(path, origin_path, filename):
                 os.path.join(des_path, filename))
 
 
+def name_from_51job(stream):
+    name = core.information_explorer.getInfoFromRestr(stream,
+                                                      u'Script([\u4E00-\u9FA5]+)\W')
+    return name
+
+
+def name_from_filename(filename):
+    firstname_list = u"李,王,张,刘,陈,杨,黄,赵,周,吴,徐,孙,朱,马,胡,郭,林,何,高,梁,郑,罗,宋,谢,唐,韩,曹,许,邓,萧,冯,曾,程,蔡,彭,潘,袁,於,董,余,苏,叶,吕,魏,蒋,田,杜,丁,沈,姜,范,江,傅,钟,卢,汪,戴,崔,任,陆,廖,姚,方,金,邱,夏,谭,韦,贾,邹,石,熊,孟,秦,阎,薛,侯,雷,白,龙,段,郝,孔,邵,史,毛,常,万,顾,赖,武,康,贺,严,尹,钱,施,牛,洪,龚,翟,由,樊,戚,季,岑,付,占,肖,舒,闫,麦,黎,童,欧".split(u',')
+    splits = core.information_explorer.getInfoFromRestr(filename, ur'[\u4e00-\u9fa5]+')
+    name = ''
+    for each in splits:
+        if each[0] in firstname_list:
+            name = each
+            break
+    return name
+
+
 def filter(processer, origin_path, filename):
     def mustjudge(d):
         return (d['email'] or d['phone'])
-
-    firstname_list = u"李，王，张，刘，陈，杨，黄，赵，周，吴，徐，孙，朱，马，胡，郭，林，何，高，梁，郑，罗，宋，谢，唐，韩，曹，许，邓，萧，冯，曾，程，蔡，彭，潘，袁，於，董，余，苏，叶，吕，魏，蒋，田，杜，丁，沈，姜，范，江，傅，钟，卢，汪，戴，崔，任，陆，廖，姚，方，金，邱，夏，谭，韦，贾，邹，石，熊，孟，秦，阎，薛，侯，雷，白，龙，段，郝，孔，邵，史，毛，常，万，顾，赖，武，康，贺，严，尹，钱，施，牛，洪，龚".split(u'，')
 
     if processer.result is False:
         path = "NotConvert"
@@ -32,20 +47,15 @@ def filter(processer, origin_path, filename):
             path = "OK"
             move_file(path, origin_path, filename)
         else:
-            name = core.information_explorer.getInfoFromRestr(
-                processer.markdown_stream,  u'Script([\u4E00-\u9FA5]+)\W')
+            name = name_from_51job(processer.markdown_stream)
             if name:
                 path = "51jobname"
                 move_file(path, origin_path, filename)
             else:
-                splits = core.information_explorer.getInfoFromRestr(
-                    filename, ur'[\u4e00-\u9fa5]+')
-                for each in splits:
-                    if each[0] in firstname_list:
-                        name = each
-                        path = "name_in_filename"
-                        move_file(path, origin_path, filename)
-                        break
+                name = name_from_filename(filename)
+                if name:
+                    path = "name_in_filename"
+                    move_file(path, origin_path, filename)
                 else:
                     path = "needaddname"
                     move_file(path, origin_path, filename)

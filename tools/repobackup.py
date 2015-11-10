41976f1346348373f926a269a9e8eb05eb958827
followcat <followcat@163.com>
2015-11-10 15:55:17 +0800

    Split account and data repo, remove unused default repo.

--- tools/repobackup.py
+++ tools/repobackup.py
@@ -8,7 +8,8 @@ import dulwich.porcelain
 
 backup_folder = webapp.settings.BACKUP_DIR
 ISOTIMEFORMAT = '%Y-%m-%d-%X'
-source_repo = webapp.settings.REPO_DB.repo
+source_repo = webapp.settings.DATA_DB.repo
+account_repo = webapp.settings.ACCOUNT_DB.repo
 
 
 def assure_path_exists(path):
@@ -18,6 +19,9 @@ def assure_path_exists(path):
 
 if __name__ == '__main__':
     backup_name = time.strftime(ISOTIMEFORMAT, time.localtime())
-    backup_path = os.path.join(backup_folder, backup_name)
-    assure_path_exists(backup_path)
-    dulwich.porcelain.clone(source_repo.path, backup_path, bare=True)
+    data_backup_path = os.path.join(backup_folder, 'data', backup_name)
+    account_backup_path = os.path.join(backup_folder, 'account', backup_name)
+    assure_path_exists(data_backup_path)
+    assure_path_exists(account_backup_path)
+    dulwich.porcelain.clone(source_repo.path, data_backup_path, bare=True)
+    dulwich.porcelain.clone(source_repo.path, account_backup_path, bare=True)
--- webapp/settings.py
+++ webapp/settings.py
@@ -6,11 +6,14 @@ import repointerface.gitinterface
 
 UPLOAD_TEMP = 'output'
 SECRET_KEY = 'SET T0 4NY SECRET KEY L1KE RAND0M H4SH'
-REPO_DB_NAME = 'repo'
 
 USER_HOME = os.path.expanduser("~")
 BACKUP_FOLDER = 'repodb_backup'
 BACKUP_DIR = os.path.join(USER_HOME, BACKUP_FOLDER)
 
-REPO_DB = repointerface.gitinterface.GitInterface(REPO_DB_NAME)
-REPO_ACCOUNT = webapp.core.account.RepoAccount(REPO_DB)
+DATA_DB_NAME = 'repo'
+DATA_DB = repointerface.gitinterface.GitInterface(DATA_DB_NAME)
+
+ACCOUNT_DB_NAME = 'account'
+ACCOUNT_DB = repointerface.gitinterface.GitInterface(ACCOUNT_DB_NAME)
+REPO_ACCOUNT = webapp.core.account.RepoAccount(ACCOUNT_DB)

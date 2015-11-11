ef3e32eb1046be2f846b57de12c85a707cb0e49f
kabessC <chenjunkai1024@outlook.com>
2015-11-11 17:14:13 +0800

    Add added info in search result page

--- webapp/server.py
+++ webapp/server.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import flask
 import flask.ext.session
-
+import jinja2.ext
 import ext.views
 
 app = flask.Flask(__name__)
 app.config.from_object('webapp.settings')
+app.jinja_env.add_extension(jinja2.ext.loopcontrols)
 ext.views.configure(app)
 
 app.secret_key = 'super secret key'

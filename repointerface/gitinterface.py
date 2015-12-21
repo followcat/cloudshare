a1ad4a278dab74e0dd9a1b82ba2ca183e91e06ac
followcat <followcat@163.com>
2015-12-21 14:25:34 +0800

    Use stage and do_commit in repo modify, remove some function.

--- repointerface/gitinterface.py
+++ repointerface/gitinterface.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 import re
-import time
 import os.path
 import subprocess
 
 import dulwich.repo
-import dulwich.index
-import dulwich.objects
 
 import utils.builtin
 
@@ -66,7 +63,7 @@ class GitInterface(object):
             ...     file.write('test')
             >>> commit_id = interface.add_files(['test_file'],
             ... b'Test commit', b'test<test@test.com>')
-            >>> interface.modify_file('test_file', b'Modify test')
+            >>> commit_id = interface.modify_file('test_file', b'Modify test')
             >>> with open('repointerface/test_repo/test_file') as file:
             ...     data = file.read()
             >>> data
@@ -77,34 +74,11 @@ class GitInterface(object):
             message = "Change %s." % filename
         if committer is None:
             committer = self.author
-        blob = dulwich.objects.Blob.from_string(stream)
-        head_commit = self.repo.get_object(self.repo.refs['HEAD'])
-        head_tree = dulwich.objects.Tree()
-        head_tree[filename] = (0o100644, blob.id)
-        self.commit(head_tree, blob, message, committer)
-        self.checkout(head_tree)
-
-    def commit(self, tree, blob, message, committer):
-        commit = dulwich.objects.Commit()
-        commit.tree = tree.id
-        commit.parents = [self.repo.refs['HEAD']]
-        commit.author = commit.committer = committer
-        commit.commit_time = commit.author_time = int(time.time())
-        tz = dulwich.objects.parse_timezone(b'-0200')[0]
-        commit.commit_timezone = commit.author_timezone = tz
-        commit.encoding = self.encoding
-        commit.message = message.encode()
-
-        self.repo.object_store.add_object(blob)
-        self.repo.object_store.add_object(tree)
-        self.repo.object_store.add_object(commit)
-        self.repo.refs['HEAD'] = commit.id
-
-    def checkout(self, tree):
-        indexfile = self.repo.index_path()
-        tree = self.repo["HEAD"].tree
-        dulwich.index.build_index_from_tree(self.repo.path, indexfile,
-                                            self.repo.object_store, tree)
+        with open(os.path.join(self.repo.path, filename), 'w') as f:
+            f.write(stream)
+        self.repo.stage([filename])
+        commit_id = self.repo.do_commit(message, committer=committer)
+        return commit_id
 
     def grep(self, restrings):
         grep_list = []

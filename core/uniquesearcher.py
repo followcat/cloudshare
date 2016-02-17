fe757a3d73c0e0b2d0f7cd10fe7978025ede2e9d
followcat <followcat@163.com>
2016-02-17 15:02:27 +0800

    Do not check phone and email is None when checkunique.

--- core/uniquesearcher.py
+++ core/uniquesearcher.py
@@ -48,8 +48,8 @@ class UniqueSearcher(object):
         if len(phone) == 0 and len(email) == 0:
             return False
         for each in self.yaml_datas.values():
-            if (phone and phone == each['phone'].encode('utf-8') and
-                    email and email == each['email'].encode('utf-8')):
+            if (phone == each['phone'].encode('utf-8') and
+                email == each['email'].encode('utf-8')):
                 return False
         else:
             return True

cde0ca45e781f45daa1894af7bca4de8b128f58f
followcat <followcat@163.com>
2016-05-24 18:02:10 +0800

    Add predator project source interface.

--- interface/base.py
+++ interface/base.py
@@ -6,13 +6,16 @@ class Interface(object):
     def __init__(self, path):
         self.path = path
 
+    def get(self, filename):
+        raise NotImplementedInterface
+
     def add(self, filename, filedata):
         raise NotImplementedInterface
 
     def modify(self, filename, filedata):
         raise NotImplementedInterface
 
-    def list(self):
+    def lsfiles(self):
         raise NotImplementedInterface
 
     def search(self):

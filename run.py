29ff7c223939cfab62b97ed3cfd83905de99397a
followcat <followcat@163.com>
2016-06-02 18:07:43 +0800

    Turn off debug mode.

--- run.py
+++ run.py
@@ -13,4 +13,4 @@ atexit.register(lambda: sched.shutdown(wait=True))
 if __name__ == '__main__':
     sched.add_job(app.config['SVC_MIN'].update, 'interval', seconds=300)
     sched.start()
-    app.run(debug=True, host='0.0.0.0', port=4888, threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=4888, threaded=True)

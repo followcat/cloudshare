9d97acac78a24ec6c27f148e8bd60c6a1256477c
followcat <followcat@163.com>
2016-06-21 18:04:39 +0800

    Remove extract_detail in predator interface.

--- interface/predator.py
+++ interface/predator.py
@@ -10,68 +10,6 @@ import pypandoc
 import utils.builtin
 import interface.base
 
-from extractor.utils_parsing import *
-
-
-
-def cloudshare_yaml_template():
-    template = {
-        'age': 0,
-        'comment': [],
-        'committer': "SCRAPPY",
-        'company': "",
-        'date': 0.,
-        'education': "",
-        'email': '',
-        'experience': [],
-        'filename': "",
-        'id': '',
-        'name': "",
-        'origin': u'猎聘爬取',
-        'phone': "",
-        'position': "",
-        'school': "",
-        'tag': [],
-        'tracking': [],
-        }
-    return template
-
-
-def extract_details(uploaded_details):
-    details = cloudshare_yaml_template()
-
-    details['date'] = uploaded_details['date']
-    details['name'] = uploaded_details['name']
-    details['id'] = uploaded_details['data-id']
-    try:
-        details['age']= re.compile('[0-9]*').match(uploaded_details['peo'][2]).group()
-        details['company'] = uploaded_details['peo'][7]
-        details['position'] = uploaded_details['peo'][6]
-    except IndexError:
-        details['age']= 0
-        details['company'] = uploaded_details['peo'][2]
-        details['position'] = uploaded_details['peo'][1]
-    details['filename'] = uploaded_details['href']
-    add_cr = lambda x:'\n'+x.group()
-    for education in re.compile(STUDIES, re.M).finditer(re.compile(PERIOD).sub(add_cr, uploaded_details['info'][0])):
-        details['school'] = education.group('school')
-        details['major'] = education.group('major')
-        details['education'] = education.group('education')
-
-    for expe in uploaded_details['info']:
-        for w in re.compile(WORKXP, re.M).finditer(re.compile(PERIOD).sub(add_cr, expe)):
-            details['experience'].append((fix_date(w.group('from')), fix_date(w.group('to')),
-                fix_name(w.group('company'))+'|'+fix_name(w.group('position'))+'('+fix_duration(w.group('duration'))+')'))
-    if u'…' in details['company']:
-        no_braket = lambda x:x.replace('(','').replace(')','')
-        escape_star = lambda x:x.replace('*','\*')
-        RE = re.compile(escape_star(no_braket(details['company'])[:-1]))
-        for xp in details['experience']:
-            if RE.match(no_braket(xp[2])):
-                details['company'] = xp[2].split('|')[0]
-                break
-    return details
-
 
 class PredatorLiteInterface(interface.base.Interface):
     
@@ -111,7 +49,7 @@ class PredatorLiteInterface(interface.base.Interface):
         data = None
         path, id_str = os.path.split(name)
         if id_str in self.yamldata:
-            data = extract_details(self.yamldata[id_str])
+            data = self.yamldata[id_str]
             return utils.builtin.dump_yaml(data)
         return data
 

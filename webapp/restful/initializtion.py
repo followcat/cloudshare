b01f66f5a8b01735c9b6ef49e1ce3181d881be6a
followcat <followcat@163.com>
2016-07-15 16:29:27 +0800

    Add company RESTful interface.

--- webapp/restful/initializtion.py
+++ webapp/restful/initializtion.py
@@ -2,6 +2,7 @@ import flask
 import flask.ext.restful
 
 from webapp.restful.account import *
+from webapp.restful.company import *
 
 
 def initialize(app):
@@ -9,3 +10,6 @@ def initialize(app):
     api.add_resource(AccountAPI, '/api/account/<string:id>', endpoint = 'account')
     api.add_resource(AccountListAPI, '/api/accountlist', endpoint = 'accountlist')
     api.add_resource(AccountHistoryAPI, '/api/accounthistory', endpoint = 'accounthistory')
+
+    api.add_resource(CompanyAPI, '/api/company/<string:name>', endpoint = 'company')
+    api.add_resource(CompanyListAPI, '/api/companylist', endpoint = 'companylist')

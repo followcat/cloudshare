10ed2f227eed6fe85886077c60db258d7ae60732
followcat <followcat@163.com>
2016-08-11 19:14:56 +0800

    Everyone can modify jobdescription now.

--- services/jobdescription.py
+++ services/jobdescription.py
@@ -82,8 +82,6 @@ class JobDescription(services.base.Service):
     def modify(self, hex_id, description, status, committer):
         filename = self.filename(hex_id)
         data = self.get(filename)
-        if data['committer'] != committer:
-            return False
         data['description'] = description
         data['status'] = status
         dump_data = yaml.safe_dump(data, allow_unicode=True)

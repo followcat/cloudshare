c46925c59c0f89189834e4353d5a257b058f8a6c
followcat <followcat@163.com>
2016-08-16 16:04:59 +0800

    Add param DBCENTER into dbscenter.

--- baseapp/centerdbs.py
+++ baseapp/centerdbs.py
@@ -1,6 +1,16 @@
+import os
 import interface.gitinterface
 import services.curriculumvitae
 
+DBCENTER_NAME = 'dbcenter'
+DBCENTER_NAMES = {'medical', 'flying'}
+
+DBCENTER = dict()
+DBCENTER_SVC_CV = dict()
+for name in DBCENTER_NAMES:
+    namepath = os.path.join(DBCENTER_NAME, name)
+    DBCENTER[name] = interface.gitinterface.GitInterface(namepath)
+    DBCENTER_SVC_CV[name] = services.curriculumvitae.CurriculumVitae(DBCENTER[name], name)
 
 DATA_DB_NAME = 'repo'
 DATA_DB = interface.gitinterface.GitInterface(DATA_DB_NAME)

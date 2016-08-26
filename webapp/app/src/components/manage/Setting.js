1daeeff6c9a6f0295adca508b1c80795d5d452df
kabessC <chenjunkai1024@outlook.com>
2016-08-26 19:08:47 +0800

    Add change password submit method.

--- webapp/app/src/components/manage/Setting.js
+++ webapp/app/src/components/manage/Setting.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
+import { message } from 'antd';
+
 import ChangePassword from '../common/ChangePassword';
 
+message.config({
+  top: 66,
+  duration: 3,
+});
+
 export default class Setting extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    //Submit fetch
+  }
+
   render() {
     return (
       <div>
         <div className="pwd-content">
-          <ChangePassword />
+          <ChangePassword onSubmit={this.handleSubmit}/>
         </div>
       </div>
     );

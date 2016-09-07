4cf7fde217cebf13d45a6fe4594e0163afee2f56
kabessC <chenjunkai1024@outlook.com>
2016-09-07 18:44:38 +0800

    Add upload container, upload file and preview request, and its component.

---
+++ webapp/app/src/components/upload/Preview.js
@@ -0,0 +1,12 @@
+'use strict';
+import React, { Component } from 'react';
+
+export default class Preview extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
---
+++ webapp/app/src/components/upload/Uploader.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import { Upload, Icon } from 'antd';
+const Dragger = Upload.Dragger;
+
+export default class Uploader extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{ width: 720, height: 180, margin: '0 auto' }}>
+          <Dragger {...this.props.uploadProps}>
+            <p className="ant-upload-drag-icon">
+              <Icon type="inbox" />
+            </p>
+            <p className="ant-upload-text">Click or drag and drop files to upload on this area</p>
+            <p className="ant-upload-hint">Support single or mutiple upload</p>
+          </Dragger>
+        </div>
+      </div>
+    );
+  }
+}

71904da3c9dca2b6e022dc4277905923eb9b09af
jeff <jeff@debian83>
2016-09-13 11:03:22 +0800

    Move jieba staffs into utils.builtin module

--- services/mining.py
+++ services/mining.py
@@ -4,9 +4,7 @@ import re
 import core.mining.lsimodel
 import core.mining.lsisimilarity
 
-import jieba
-import jieba.posseg
-
+from utils.builtin import jieba_cut, pos_extract
 
 REJECT = re.compile('(('+')|('.join([
     u'中文', u'日期', u'汽车',
@@ -54,61 +52,6 @@ FLAGS = ['x', # spaces
          'z', # State word
          #'ns', # city and country
         ]
-
-def jieba_cut(text, pos=False):
-    """
-        >>> from services.mining import *
-        >>> s = "测试计量技术及仪器"
-        >>> for _w in jieba_cut(s):
-        ...     print(_w.encode('utf-8'))
-        测试
-        计量
-        技术
-        及
-        仪器
-        >>> for _w in jieba_cut(s, pos=True):
-        ...     print(_w.encode('utf-8'))
-        测试/vn
-        计量/n
-        技术/n
-        及/c
-        仪器/n
-    """
-    if pos:
-        return jieba.posseg.cut(text)
-    return jieba.cut(text)
-
-def pos_extract(words, flags):
-    """
-        >>> from services.mining import *
-        >>> s = "◆负责产品环境、电磁兼容、可靠性、安规等测试；"
-        >>> words = list(jieba_cut(s, pos=True))
-        >>> for _w in words:
-        ...     print(_w.encode('utf-8'))
-        ◆/x
-        负责/v
-        产品/n
-        环境/n
-        、/x
-        电磁兼容/l
-        、/x
-        可靠性/n
-        、/x
-        安规/nr
-        等/u
-        测试/vn
-        ；/x
-        >>> words = pos_extract(words, FLAGS)
-        >>> for _w in words:
-        ...     print(_w.encode('utf-8'))
-        负责
-        产品
-        环境
-        电磁兼容
-        可靠性
-        测试
-    """
-    return [word.word for word in words if word.flag not in flags]
 
 def re_sub(reg, sub, text):
     """

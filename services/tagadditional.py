a6bcab35a947433b6fe76ef945a208508a75e04c
followcat <followcat@163.com>
2016-09-18 18:50:59 +0800

    Modify tagadditional service to support cvstorage.

--- services/tagadditional.py
+++ services/tagadditional.py
@@ -9,47 +9,43 @@ class TagsCurriculumVitae(object):
     config_file = 'config.json'
     ids_file = 'names.json'
 
-    def __init__(self, name, path, additionals):
+    def __init__(self, name, path, cvstorage):
         self.name = name
         self.path = os.path.join(path, name)
-        self.additionals = additionals
         self.cvids = dict()
         self.config = dict()
+        self.cvstorage = cvstorage
         try:
             self.load()
         except IOError:
             pass
 
-    def setup(self, additionals, selected):
+    def setup(self, selected):
         if not os.path.exists(self.path):
             os.makedirs(self.path)
-        self.additionals = dict([(a.name, a) for a in additionals])
-        self.config['additionals'] = set(self.additionals.keys())
         self.config['selected'] = selected
         self.update()
 
     def load(self):
         self.config = utils.builtin.load_json(self.path, self.config_file)
-        self.cvids = utils.builtin.load_json(self.path, self.ids_file)
+        self.cvids = set(utils.builtin.load_json(self.path, self.ids_file))
 
     def save(self):
         utils.builtin.save_json(self.config, self.path, self.config_file)
         utils.builtin.save_json(self.cvids, self.path, self.ids_file)
 
     def update(self):
-        for name in self.config['additionals']:
-            svc_cv = self.additionals[name]
-            for yamlname in svc_cv.yamls():
-                if not self.exists(yamlname):
-                    yamlinfo = svc_cv.getyaml(yamlname)
-                    key = self.config['selected']['key']
-                    values_set = self.config['selected']['value']
-                    if key in yamlinfo:
-                        keyset = set()
-                        for each in yamlinfo[key].values():
-                            keyset.update(each)
-                        if keyset.intersection(values_set):
-                            self._add(yamlname, name)
+        for yamlname in self.cvstorage.yamls():
+            if not self.exists(yamlname):
+                yamlinfo = self.cvstorage.getyaml(yamlname)
+                key = self.config['selected']['key']
+                values_set = self.config['selected']['value']
+                if key in yamlinfo:
+                    keyset = set()
+                    for each in yamlinfo[key].values():
+                        keyset.update(each)
+                    if keyset.intersection(values_set):
+                        self._add(yamlname, name)
         self.save()
 
     def exists(self, name):
@@ -58,7 +54,7 @@ class TagsCurriculumVitae(object):
 
     def _add(self, name, svccv_name):
         id = core.outputstorage.ConvertName(name).base
-        self.cvids[id] = svccv_name
+        self.cvids.add(id)
 
     def yamls(self):
         for id in self.cvids:
@@ -68,29 +64,18 @@ class TagsCurriculumVitae(object):
         for id in self.cvids:
             yield core.outputstorage.ConvertName(id).md
 
-    def id_svccv(self, name):
-        id = core.outputstorage.ConvertName(name).base
-        svccv_name = self.cvids[id]
-        if svccv_name not in self.additionals:
-            raise Exception('Not loaded db: %s.' % svccv_name)
-        return self.additionals[svccv_name]
-
     def getmd(self, name):
-        svccv = self.id_svccv(name)
-        return svccv.getmd(name)
+        return self.cvstorage.getmd(name)
 
     def getyaml(self, name):
-        svccv = self.id_svccv(name)
-        return svccv.getyaml(name)
+        return self.cvstorage.getyaml(name)
 
     def gethtml(self, name):
-        svccv = self.id_svccv(name)
-        return svccv.gethtml(name)
+        return self.cvstorage.gethtml(name)
 
     def datas(self):
-        for id in self.cvids:
-            svccv = self.id_svccv(name)
-            text = svccv.getmd(id)
+        for name in self.names():
+            text = self.cvstorage.getmd(name)
             yield name, text
 
     @property

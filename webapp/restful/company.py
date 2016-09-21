e1d42939c43b4a484db8ea1cfd044887ae79098e
kabessC <chenjunkai1024@outlook.com>
2016-09-21 16:58:11 +0800

    Modify company api.

--- webapp/restful/company.py
+++ webapp/restful/company.py
@@ -11,19 +11,21 @@ class CompanyAPI(Resource):
     def __init__(self):
         self.svc_company = flask.current_app.config['SVC_CO']
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('introduction', type = str, location = 'json')
+        self.reqparse.add_argument('coname', location = 'json')
+        self.reqparse.add_argument('introduction', location = 'json')
         super(CompanyAPI, self).__init__()
 
     def get(self, name):
         result = self.svc_company.company(name)
         return { 'result': result }
 
-    def post(self, name):
+    def post(self):
         args = self.reqparse.parse_args()
+        coname = args['coname']
         introduction = args['introduction']
         user = flask.ext.login.current_user
-        result = self.svc_company.add(name, introduction, user.id)
-        return { 'result': result }
+        result = self.svc_company.add(coname, introduction, user.id)
+        return { 'code': 200, 'data': result, 'message': 'Create new company successed.' }
 
 
 class CompanyListAPI(Resource):
@@ -36,4 +38,8 @@ class CompanyListAPI(Resource):
 
     def get(self):
         result = self.svc_company.names()
-        return { 'code': 200, 'data': result }
+        data = []
+        for coname in result:
+            co = self.svc_company.company(coname)
+            data.append(co)
+        return { 'code': 200, 'data': data }

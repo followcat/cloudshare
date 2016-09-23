3fea07d3613ca376629fdb60a2ea4214e942f48b
kabessC <chenjunkai1024@outlook.com>
2016-09-23 18:55:20 +0800

    Add data load function.

--- webapp/app/src/entry/fastmatching.entry.js
+++ webapp/app/src/entry/fastmatching.entry.js
@@ -1,11 +1,15 @@
 'use strict';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import enUS from 'antd/lib/locale-provider/en_US';
+import { LocaleProvider } from 'antd';
 
 import FastMatching from '../containers/FastMatching';
 
 ReactDOM.render(
-  <FastMatching />,
+  <LocaleProvider locale={enUS}>
+    <FastMatching />
+  </LocaleProvider>,
   document.getElementById('app')
 );

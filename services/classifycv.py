418d72f35fdc21d4ce74f9840103f31ffd3a84c9
followcat <followcat@163.com>
2016-10-08 18:48:30 +0800

    Add services multiclsify, and init classifies from MultiClassify, use its update to update all classies fast.

--- services/classifycv.py
+++ services/classifycv.py
@@ -14,16 +14,17 @@ class ClassifyCV(services.simulationcv.SimulationCV):
         super(ClassifyCV, self).__init__(classifypath, path, cvstorage)
         self.name = name
 
-    def setup(self):
+    def setup(self, update=True):
         if not os.path.exists(self.path):
             os.makedirs(self.path)
             self.config['classify'] = self.name
-            self.update()
+            if update:
+                self.update()
 
     def update(self):
         for y in self.cvstorage.yamls():
             if not self.exists(y):
-                info = SVC_CV_REPO.getyaml(y)
+                info = self.cvstorage.getyaml(y)
                 if self.name in info['classify']:
                     self._add(y)
         self.save()

8a0d6eec50f92fbfc2a0e934c991388914d32578
kabessC <chenjunkai1024@outlook.com>
2016-10-10 16:30:53 +0800

    Add experience process function.

--- webapp/restful/search.py
+++ webapp/restful/search.py
@@ -26,29 +26,30 @@ class SearchbyTextAPI(Resource):
         cur_page = args['page']
         result = self.svc_mult_cv.search(text)
         yaml_result = self.svc_mult_cv.search_yaml(text)
+        results = list(set(result+yaml_result))
         count = 20
-        datas, pages = self.paginate(result, yaml_result, cur_page, count)
+        datas, pages = self.paginate(results, cur_page, count)
         return { 
             'code': 200,
             'data': {
                 'keyword': text,
                 'datas': datas,
                 'pages': pages,
-                'totals': len(result),
+                'totals': len(results),
             } 
         }
 
-    def paginate(self, result, yaml_result, cur_page, eve_count):
+    def paginate(self, results, cur_page, eve_count):
         if not cur_page:
             cur_page = 1
-        sum = len(result)
+        sum = len(results)
         if sum%eve_count != 0:
             pages = sum/eve_count + 1
         else:
             pages = sum/eve_count
         datas = []
         names = []
-        for each in (result+yaml_result)[(cur_page-1)*eve_count:cur_page*eve_count]:
+        for each in results[(cur_page-1)*eve_count:cur_page*eve_count]:
             base, suffix = os.path.splitext(each)
             name = core.outputstorage.ConvertName(base)
             if name not in names:
@@ -64,18 +65,21 @@ class SearchbyTextAPI(Resource):
                 'author': yaml_info['committer'],
                 'time': utils.builtin.strftime(yaml_info['date']),
             }
-            if len(yaml_info['experience']) > 0:
-                ex_company = yaml_info['experience']['company']
-                ex_position = yaml_info['experience']['position']
-            if len(ex_position) > 0:
-                for position in ex_position:
-                    for company in ex_company:
-                        if position['at_company'] == company['id']:
-                            position['company'] = company['name']
-                            if 'business' in company:
-                                position['business'] = company['business']
-                yaml_info['experience'] = ex_position
-            else: 
-                yaml_info['experience'] = ex_company
+            yaml_info['experience'] = self.experience_process(yaml_info['experience'])
             datas.append({ 'cv_id': name, 'yaml_info': yaml_info, 'info': info})
         return datas, pages
+
+    def experience_process(self, experience):
+        ex_company = experience['company'] if len(experience) and 'company' in experience else []
+        ex_position = experience['position'] if len(experience) and 'position' in experience else []
+
+        if len(ex_position) > 0:
+            for position in ex_position:
+                for company in ex_company:
+                    if position['at_company'] == company['id']:
+                        position['company'] = company['name']
+                        if 'business' in company:
+                            position['business'] = company['business']
+            return ex_position
+        else:
+            return ex_company

6599b11b61e0bb41106b39bc4b7d223429b5ab36
kabessC <chenjunkai1024@outlook.com>
2016-10-14 16:36:44 +0800

    Modify SearchResultItem component style, and add uploader time.

--- webapp/app/src/components/common/searchresult/SearchResultItem.js
+++ webapp/app/src/components/common/searchresult/SearchResultItem.js
@@ -14,6 +14,7 @@ export default class SearchResultItem extends Component {
       checked: false,
     };
     this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.renderNameText = this.renderNameText.bind(this);
   }
 
   handleCheckboxChange(e) {
@@ -21,6 +22,12 @@ export default class SearchResultItem extends Component {
       id: e.target['data-id'],
       name: e.target['data-name'],
     });
+  }
+
+  renderNameText() {
+    const name = this.props.yaml_info.name ? this.props.yaml_info.name : this.props.yaml_info.id,
+          origin = this.props.yaml_info.origin ? ' - ' + this.props.yaml_info.origin : '';
+    return name + origin;
   }
 
   render() {
@@ -48,8 +55,14 @@ export default class SearchResultItem extends Component {
                 checked={props.selection.findIndex(v => v.get('id') === props.yaml_info.id ) > -1 ? true : false}
               />
             </Col>}
-            <Col span={this.props.type === 'default' ? 5 : 4}>
-              <a href={`/show/${props.cv_id}`} target="_blank">{props.yaml_info.name ? props.yaml_info.name : props.yaml_info.id}</a>
+            <Col span={this.props.type === 'default' ? 4 : 3}>
+              <a
+                href={`/show/${props.cv_id}`}
+                target="_blank"
+                style={{ color: this.props.gradient[parseInt(props.info.match*100)] }}
+              >
+                {this.renderNameText()}
+              </a>
             </Col>
             <Col span={1}>{props.yaml_info.gender}</Col>
             <Col span={1}>{props.yaml_info.age}</Col>
@@ -57,8 +70,8 @@ export default class SearchResultItem extends Component {
             <Col span={3}>{props.yaml_info.education}</Col>
             <Col span={3}>{props.yaml_info.school}</Col>
             <Col span={3}>{props.yaml_info.position}</Col>
-            <Col span={3}>{props.yaml_info.company}</Col>
-            <Col span={3}>{props.info.author}</Col>
+            <Col span={4}>{props.yaml_info.company}</Col>
+            <Col span={3}>{props.info.author ? props.info.author : 'null'}, {props.info.time.split(' ')[0].replace(/\-/g, '/')}</Col>
           </Row>
         </div>
         <div className="extend-info">

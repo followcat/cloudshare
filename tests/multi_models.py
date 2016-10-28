f65dc0a95d1fb22fa418bf18e6465a74a04859d2
jeff <jeff@debian83>
2016-10-28 18:27:34 +0800

    Update function to build uav model more easy

--- tests/multi_models.py
+++ tests/multi_models.py
@@ -6,17 +6,21 @@ from baseapp.datadbs import *
 
 
 kgr_file = 'tests/known_good_jd_cv_mapping.yaml'
+uav_file = 'tests/uav.yaml'
 
 with open(kgr_file) as f:
     datas = yaml.load(f)
 
-def build_lsimodel(path, slicer, names=None, texts=None):
+with open(uav_file) as f:
+    datas.update(yaml.load(f))
+
+def build_lsimodel(path, slicer, names=None, texts=None, no_above=2./2):
     topics = 100
     if len(names) < 10:
         topics = 5
-    elif 10 <= len(names) < 30:
+    elif 10 <= len(names) < 40:
         topics = 10
-    m = core.mining.lsimodel.LSImodel(path, topics=topics, slicer=slicer)
+    m = core.mining.lsimodel.LSImodel(path, no_above=no_above, topics=topics, slicer=slicer)
     try:
         m.load()
     except IOError:
@@ -24,13 +28,19 @@ def build_lsimodel(path, slicer, names=None, texts=None):
         m.save()
     return m
 
+def get_cv_md(id):
+    t = SVC_CV_REPO.getmd(id)
+    if t is None:
+        t = SVC_CV_STO.getmd(id)
+    return t
+
 def build_model(jds, name=None, path='tests/lsimodel'):
     names = []
     texts = []
     for jd_id in jds:
         cvs = datas[jd_id]
         for id in cvs:
-            t = SVC_CV_REPO.getmd(id)
+            t = get_cv_md(id)
             if t is None:
                 continue
             names.append(id)

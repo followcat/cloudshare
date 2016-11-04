4558e14feee4ce373b239ae48b45a25f1628b899
kabessC <chenjunkai1024@outlook.com>
2016-11-04 16:31:33 +0800

    Add promise support on IE and old version browser.

--- webapp/app/src/entry/listjd.entry.js
+++ webapp/app/src/entry/listjd.entry.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
+import 'babel-polyfill';
+
 import enUS from 'antd/lib/locale-provider/en_US';
 import { LocaleProvider } from 'antd';
 

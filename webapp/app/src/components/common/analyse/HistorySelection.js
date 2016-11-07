6b3fd02ac6f8c38c90bd5f5ad798d99c53214644
kabessC <chenjunkai1024@outlook.com>
2016-11-07 15:27:39 +0800

    Fixed history selected row and the selection storage does not synchronize problem.

--- webapp/app/src/components/common/analyse/HistorySelection.js
+++ webapp/app/src/components/common/analyse/HistorySelection.js
@@ -12,17 +12,31 @@ export default class HistorySelection extends Component {
 
     this.state = {
       visible: false,
+      selectedRowKeys: [],
       data: [],
     };
 
     this.handleClick = this.handleClick.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
   }
 
   handleClick() {
+    const historyStorage = History.read(),
+          selection = this.props.selection;
+    let selectData = [];
+
+    historyStorage.forEach((item, index) => {
+      selection.forEach((value) => {
+        if (item.id === value.get('id')) {
+          selectData.push(index);
+        }
+      });
+    });
     this.setState({
       visible: true,
-      data: History.read(),
+      data: historyStorage,
+      selectedRowKeys: selectData,
     });
   }
 
@@ -32,25 +46,32 @@ export default class HistorySelection extends Component {
     });
   }
 
+  handleSelectChange(selectedRowKeys) {
+    this.setState({
+      selectedRowKeys: selectedRowKeys,
+    });
+  }
+
   render() {
     const columns = [
       {
         title: 'Resume ID',
         dataIndex: 'id',
         width: '30%',
+        key: 'id',
       }, {
         title: 'Name',
         dataIndex: 'name',
         width: '50%',
+        key: 'name',
       },
     ];
 
     const rowSelection = {
       type: 'checkbox',
-      getCheckboxProps: record => ({
-        defaultChecked: this.props.selection.findIndex(v => v.get('id') === record.id ) > -1 ? true : false,
-      }),
-      onSelect: (record, selected, selectedRows) => {
+      selectedRowKeys: this.state.selectedRowKeys,
+      onChange: this.handleSelectChange,
+      onSelect: (record) => {
         this.props.onToggleSelection({
           id: record.id,
           name: record.name

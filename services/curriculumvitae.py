134919e37fe8053285d6318129986f2e1a7dbad9
followcat <followcat@163.com>
2016-11-09 17:49:10 +0800

    Move issue update_name from simulationcv to curriculumvitae.

--- services/curriculumvitae.py
+++ services/curriculumvitae.py
@@ -238,6 +238,64 @@ class CurriculumVitae(services.base.Service):
             self._nums = len(list(self.yamls()))
         return self._nums
 
+    @utils.issue.fix_issue('issues/update_name.rst')
+    def updateinfo(self, id, key, value, committer):
+        data = None
+        projectinfo = self.getinfo(id)
+        baseinfo = self.getyaml(id)
+        if projectinfo is not None and (key in projectinfo or key in baseinfo):
+            data = { key: value }
+            if key == 'tag':
+                data = self.addtag(id, value, committer)
+            elif key == 'tracking':
+                data = self.addtracking(id, value, committer)
+            elif key == 'comment':
+                data = self.addcomment(id, value, committer)
+            else:
+                projectinfo[key] = value
+                self.saveinfo(id, projectinfo,
+                              'Update %s key %s.' % (id, key), committer)
+        return data
+
+    def _infoframe(self, value, username):
+        data = {'author': username,
+                'content': value,
+                'date': time.strftime('%Y-%m-%d %H:%M:%S')}
+        return data
+
+    def addtag(self, id, tag, committer):
+        assert self.exists(id)
+        info = self.getinfo(id)
+        data = self._infoframe(tag, committer)
+        info['tag'].insert(0, data)
+        self.saveinfo(id, info, 'Add %s tag.'%id, committer)
+        return data
+
+    def addcomment(self, id, comment, committer):
+        assert self.exists(id)
+        info = self.getinfo(id)
+        data = self._infoframe(comment, committer)
+        info['comment'].insert(0, data)
+        self.saveinfo(id, info, 'Add %s comment.'%id, committer)
+        return data
+
+    def addtracking(self, id, tracking, committer):
+        assert self.exists(id)
+        info = self.getinfo(id)
+        data = self._infoframe(tracking, committer)
+        info['tracking'].insert(0, data)
+        self.saveinfo(id, info, 'Add %s tracking.'%id, committer)
+        return data
+
+    def getinfo(self, id):
+        return self.getyaml(id)
+
+    def saveinfo(self, id, info, message, committer):
+        name = core.outputstorage.ConvertName(id).yaml
+        dumpinfo = yaml.dump(info, Dumper=utils._yaml.SafeDumper,
+                             allow_unicode=True, default_flow_style=False)
+        self.interface.modify(name, dumpinfo, message=message, committer=committer)
+
     @property
     def ids(self):
         return [os.path.splitext(f)[0]

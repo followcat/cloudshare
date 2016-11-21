7b5cbdb8aca8f6791d71292ac03e53126b45b73a
followcat <followcat@163.com>
2016-11-21 17:30:07 +0800

    Modify CVStorageSync input param in datadbs.

--- baseapp/datadbs.py
+++ baseapp/datadbs.py
@@ -25,7 +25,7 @@ if os.path.exists(RAW_DIR):
     for name in os.listdir(RAW_DIR):
         namepath = os.path.join(RAW_DIR, name)
         RAW_DB[name] = interface.predator.PredatorInterface(namepath)
-SVC_ADD_SYNC = services.cvstoragesync.CVStorageSync(SVC_CV_STO, RAW_DB)
+SVC_ADD_SYNC = services.cvstoragesync.CVStorageSync(SVC_PEO_STO, SVC_CV_STO, RAW_DB)
 
 SVC_MULT_CLSIFY = services.multiclsify.MultiClassify(SVC_CV_STO)
 SVC_CLS_CV = SVC_MULT_CLSIFY.classifies

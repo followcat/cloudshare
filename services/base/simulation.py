08cb43bdce35a13de05ac5274419165b389c49ba
followcat <followcat@163.com>
2016-11-30 15:21:38 +0800

    Only Simulation can modify yaml info.

--- services/base/simulation.py
+++ services/base/simulation.py
@@ -1,9 +1,11 @@
 import os
+import time
+
 import yaml
 import ujson
+
 import utils._yaml
 import utils.builtin
-
 import core.outputstorage
 import services.base.storage
 
@@ -84,6 +86,61 @@ class Simulation(services.base.storage.BaseStorage):
             info = self.getinfo(id)
             yaml.update(info)
         return yaml
+
+    @utils.issue.fix_issue('issues/update_name.rst')
+    def updateinfo(self, id, key, value, committer):
+        data = None
+        projectinfo = self.getinfo(id)
+        baseinfo = self.getyaml(id)
+        if projectinfo is not None and (key in projectinfo or key in baseinfo):
+            data = { key: value }
+            if key == 'tag':
+                data = self.addtag(id, value, committer)
+            elif key == 'tracking':
+                data = self.addtracking(id, value, committer)
+            elif key == 'comment':
+                data = self.addcomment(id, value, committer)
+            else:
+                projectinfo[key] = value
+                self.saveinfo(id, projectinfo,
+                              'Update %s key %s.' % (id, key), committer)
+        return data
+
+    def _infoframe(self, value, username):
+        data = {'author': username,
+                'content': value,
+                'date': time.strftime('%Y-%m-%d %H:%M:%S')}
+        return data
+
+    def addtag(self, id, tag, committer):
+        assert self.exists(id)
+        info = self.getinfo(id)
+        data = self._infoframe(tag, committer)
+        info['tag'].insert(0, data)
+        self.saveinfo(id, info, 'Add %s tag.'%id, committer)
+        return data
+
+    def addcomment(self, id, comment, committer):
+        assert self.exists(id)
+        info = self.getinfo(id)
+        data = self._infoframe(comment, committer)
+        info['comment'].insert(0, data)
+        self.saveinfo(id, info, 'Add %s comment.'%id, committer)
+        return data
+
+    def addtracking(self, id, tracking, committer):
+        assert self.exists(id)
+        info = self.getinfo(id)
+        data = self._infoframe(tracking, committer)
+        info['tracking'].insert(0, data)
+        self.saveinfo(id, info, 'Add %s tracking.'%id, committer)
+        return data
+
+    def saveinfo(self, id, info, message, committer):
+        name = core.outputstorage.ConvertName(id).yaml
+        dumpinfo = yaml.dump(info, Dumper=utils._yaml.SafeDumper,
+                             allow_unicode=True, default_flow_style=False)
+        self.interface.modify(name, dumpinfo, message=message, committer=committer)
 
     def search(self, keyword):
         results = set()

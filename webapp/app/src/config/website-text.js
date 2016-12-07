a4b6e7831ac8a3bd6e7951bd9e771c712077df4f
kabessC <chenjunkai1024@outlook.com>
2016-12-07 18:41:31 +0800

    Add website text configuration file.

---
+++ webapp/app/src/config/website-text.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const text = {
+
+  // 简体中文
+  'zhCN': {
+    SEARCH: '搜索',
+    MATCH: '匹配',
+    PROJECT_MANAGEMENT: '项目管理',
+    JOB_DESCRIPTION_MANAGEMENT: '职位管理',
+    CUSTOMER_MANAGEMENT: '客户管理',
+    OWN_CUSTOMER_MANAGEMENT: '已拥有客户',
+    DEVELOPMENT_CUSTOMER_MANAGEMENT: '待开发客户',
+  },
+
+  // 待扩展语言
+};
+
+module.exports = text;

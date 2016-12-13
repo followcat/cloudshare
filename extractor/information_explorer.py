e5847351685dc5705ab64a152cdd47060038d7ba
followcat <followcat@163.com>
2016-12-13 18:42:01 +0800

    Add unique_id as selected option in catch.

--- extractor/information_explorer.py
+++ extractor/information_explorer.py
@@ -243,7 +243,9 @@ def get_age(stream):
     return result
 
 
-all_selected = ('name', 'originid', 'age', 'phone', 'email', 'education', 'experience', 'expectation', 'classify')
+all_selected = ('name', 'originid', 'age', 'phone', 'email',
+                'education', 'experience', 'expectation',
+                'classify', 'unique_id')
 
 def catch_selected(stream, selected, name=None):
     assert set(selected).issubset(set(all_selected))
@@ -265,6 +267,8 @@ def catch_selected(stream, selected, name=None):
     if 'expectation' in selected:
         info_dict.update(get_expectation(stream))   # expectation, current, gender, marital_status,
                                                     # age
+    if 'unique_id' in selected:
+        extractor.unique_id.unique_id(info_dict)
     if 'classify' in selected and 'classify' not in info_dict:
         experience = get_experience(stream, name)
         info_dict["classify"] = get_classify(experience['experience'])

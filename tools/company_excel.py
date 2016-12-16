4b917817e89e2983b83fa7d444d1d557036c15a6
followcat <followcat@163.com>
2016-12-16 16:29:45 +0800

    Update company_excel tool.

--- tools/company_excel.py
+++ tools/company_excel.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+
 import yaml
 
 import core.basedata
@@ -9,34 +11,41 @@ from extractor.information_explorer import *
 def outputdict(path):
     import xlrd
     data = xlrd.open_workbook(path)
-    table = data.sheets()[0]
     l = list()
-    for r in range(table.nrows):
-        if r > 1:
-            d = dict()
-            for i in range(len(table.row_values(1))):
-                d[table.row_values(1)[i]] = table.row_values(r)[i]
-            l.append(d)
+    for table in data.sheets():
+        for r in range(table.nrows):
+            if r > 0:
+                d = dict()
+                for i in range(len(table.row_values(0))):
+                    d[table.row_values(0)[i]] = table.row_values(r)[i]
+                l.append(d)
     return l
 
 def chs_to_eng(d):
+    def loose(d, words):
+        for word in words:
+            if word in d:
+                return d[word]
+        else:
+            return ''
     nd = dict()
-    nd['name'] = d[u'公司全称']
-    nd['product'] = d[u'产品']
-    nd['position'] = d[u'Open positions']   #[、，,]
-    nd['website'] = d[u'网站']
-    nd['clientcontact'] = d[u'联系人']       #
-    nd['conumber'] = d[u'座机']
-    nd['caller'] = d[u'陌拜人']              #
-    nd['address'] = d[u'地址']
-    nd['introduction'] = d[u'公司概况']
-    nd['progress'] = d[u'陌拜情况']          #;\n；
-    nd['updatednumber'] = d[u'联系电话']     #、；
-    nd['email'] = d[u'邮箱']
+    nd['district'] =        loose(d, [u'区域'])
+    nd['name'] =            loose(d, [u'公司全称'])
+    nd['introduction'] =    loose(d, [u'公司概况'])
+    nd['relatedcompany'] =  loose(d, [u'关联公司'])             #[、，,]
+    nd['product'] =         loose(d, [u'产品'])
+    nd['address'] =         loose(d, [u'公司地址', u'地址'])
+    nd['website'] =         loose(d, [u'公司网站', u'网站'])
+    nd['position'] =        loose(d, [u'近期招聘职位'])          #[、，,]
+    nd['updatednumber'] =   loose(d, [u'联系电话'])              #、；
+    nd['email'] =           loose(d, [u'邮箱'])
+    nd['clientcontact'] =   loose(d, [u'联系人'])                #
+    nd['conumber'] =        loose(d, [u'座机'])
+    nd['caller'] =          loose(d, [u'跟进人', u'陌拜人'])      #
+    nd['progress'] =        loose(d, [u'跟进情况', u'陌拜情况'])   #;\n；
     return nd
 
 def reformat(item):
-    import re
     if not isinstance(item['position'], list):
         item['position'] = [e for e in re.split(u'[、，,]', item['position']) if e]
     if not isinstance(item['clientcontact'], list):
@@ -78,16 +87,18 @@ def convert_repo(SVC_CO_REPO):
             fp.write(yaml.safe_dump(do.metadata, allow_unicode=True))
 
 
-def init_simid(SVC_CO_SIM, SVC_CO_REPO):
-    for each in SVC_CO_REPO.ids:
+def init_simid(SVC_CO_SIM, SVC_CO_REPO, ids):
+    for each in ids:
         info = SVC_CO_REPO.getyaml(each)
         metadata = catch_coinfo(info, info['name'])
         do = core.basedata.DataObject(metadata, metadata)
         SVC_CO_SIM.add(do)
 
+
 def init_siminfo(SVC_CO_SIM, d):
     dos = process(d)
-    for key in ('position', 'clientcontact', 'caller', 'progress', 'updatednumber'):
+    for key in ('relatedcompany', 'position', 'clientcontact',
+                'caller', 'progress', 'updatednumber'):
         for chs in d:
             eng = chs_to_eng(chs)
             format_eng = reformat(eng)

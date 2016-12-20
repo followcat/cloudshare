38edf50ceae50e658524a88c9139ee4dbbca5c6c
kabessC <chenjunkai1024@outlook.com>
2016-12-20 16:00:26 +0800

    Add ProjectManagement entrance.

---
+++ webapp/app/src/views/ProjectManagement/index.js
@@ -0,0 +1,14 @@
+'use strict';
+import './index.less';
+
+import Layout from './component/Layout';
+import JobDescription from './component/JobDescription';
+import OwnCustomer from './component/OwnCustomer';
+import DevelopmentalCustomer from './component/DevelopmentalCustomer';
+
+export {
+  Layout,
+  JobDescription,
+  OwnCustomer,
+  DevelopmentalCustomer
+};

40bde7ce93d7a85d7d8a50685a9caaf76c715256
followcat <followcat@163.com>
2016-12-29 14:28:15 +0800

    Add excel compare method in company and simulation company service.

--- services/simulationco.py
+++ services/simulationco.py
@@ -1,7 +1,10 @@
 import ujson
 
+import utils.companyexcel
+import core.outputstorage
 import services.company
 import services.base.simulation
+import extractor.information_explorer
 
 
 class SimulationCO(services.base.simulation.Simulation,
@@ -24,6 +27,31 @@ class SimulationCO(services.base.simulation.Simulation,
     def __init__(self, path, name, cvstorage, iotype='git'):
         super(SimulationCO, self).__init__(path, name, cvstorage, iotype)
         self._customers = None
+
+    def compare_excel(self, stream, committer):
+        datas = list()
+        output = list()
+        excels = utils.companyexcel.convert(stream)
+        for excel in excels:
+            metadata = extractor.information_explorer.catch_coinfo(excel, excel['name'])
+            data = core.basedata.DataObject(metadata, excel)
+            datas.append(data)
+        for key in self.list_item:
+            for excel in excels:
+                id = extractor.unique_id.company_id(excel['name'])
+                caller = committer
+                existvalues = list()
+                if self.exists(id):
+                    info = self.getyaml(id)
+                    if excel['caller']:
+                        caller = excel['caller'][0]
+                    existvalues = [v['content'] for v in info[key]]
+                for value in excel[key]:
+                    if value in existvalues:
+                        continue
+                    existvalues.append(value)
+                    output.append(('followup', (id, key, value, caller)))
+        return output
 
     def addcustomer(self, id, user):
         result = False

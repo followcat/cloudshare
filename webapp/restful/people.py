b5f1e634b548b4b7382eb8315739751ca23d6f2b
followcat <followcat@163.com>
2017-01-09 15:36:35 +0800

    Rename People RESTful API, update its param name.

--- webapp/restful/people.py
+++ webapp/restful/people.py
@@ -4,34 +4,35 @@ from flask.ext.restful import reqparse
 from flask.ext.restful import Resource
 
 
-class PeopleByUniqueIDAPI(Resource):
+class PeopleAPI(Resource):
 
     decorators = [flask.ext.login.login_required]
 
     def __init__(self):
-        super(PeopleByUniqueIDAPI, self).__init__()
+        super(PeopleAPI, self).__init__()
         self.svc_mult_cv = flask.current_app.config['SVC_MULT_CV']
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('date', type = str, location = 'json')
         self.reqparse.add_argument('project', type = str, location = 'json')
-        self.reqparse.add_argument('uniqueid', type = str, location = 'json')
+        self.reqparse.add_argument('unique_id', type = str, location = 'json')
 
     def post(self):
-        uniqueid = args['uniqueid']
+        args = self.reqparse.parse_args()
+        unique_id = args['unique_id']
         projectname = args['project']
         project = self.svc_mult_cv.getproject(projectname)
-        peopleinfo = project.peo_getyaml(uniqueid)
+        peopleinfo = project.peo_getyaml(unique_id)
         return { 'code': 200, 'data': peopleinfo }
 
     def put(self):
         args = self.reqparse.parse_args()
         user = flask.ext.login.current_user
-        id = args['id']
+        unique_id = args['unique_id']
         projectname = args['project']
         update_info = args['update_info']
         project = self.svc_mult_cv.getproject(projectname)
         for key, value in update_info.iteritems():
-            data = project.peo_updateyaml(id, key, value, user.id)
+            data = project.peo_updateyaml(unique_id, key, value, user.id)
             if data is not None:
                 response = { 'code': 200, 'data': data, 'message': 'Update information success.' }
             else:
@@ -42,13 +43,13 @@ class PeopleByUniqueIDAPI(Resource):
     def delete(self):
         args = self.reqparse.parse_args()
         user = flask.ext.login.current_user
-        id = args['id']
+        unique_id = args['unique_id']
         date = args['date']
         projectname = args['project']
         update_info = args['update_info']
         project = self.svc_mult_cv.getproject(projectname)
         for key, value in update_info.iteritems():
-            data = project.peo_deleteyaml(id, key, value, user.id, date)
+            data = project.peo_deleteyaml(unique_id, key, value, user.id, date)
             if data is not None:
                 response = { 'code': 200, 'data': data, 'message': 'Delete information success.' }
             else:
@@ -57,22 +58,23 @@ class PeopleByUniqueIDAPI(Resource):
         return response
 
 
-class PeopleByIDAPI(Resource):
+class PeopleByCVAPI(Resource):
 
     decorators = [flask.ext.login.login_required]
 
     def __init__(self):
-        super(PeopleByIDAPI, self).__init__()
+        super(PeopleByCVAPI, self).__init__()
         self.svc_mult_cv = flask.current_app.config['SVC_MULT_CV']
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('id', type = str, location = 'json')
+        self.reqparse.add_argument('cv_id', type = str, location = 'json')
         self.reqparse.add_argument('project', type = str, location = 'json')
 
     def post(self):
-        id = args['id']
+        args = self.reqparse.parse_args()
+        cv_id = args['cv_id']
         projectname = args['project']
         project = self.svc_mult_cv.getproject(projectname)
-        yamlinfo = project.cv_getyaml(id)
-        uniqueid = yamlinfo['unique_id']
-        peopleinfo = project.peo_getyaml(uniqueid)
+        yamlinfo = project.cv_getyaml(cv_id)
+        unique_id = yamlinfo['unique_id']
+        peopleinfo = project.peo_getyaml(unique_id)
         return { 'code': 200, 'data': peopleinfo }

1f74e35351fa3960d2eddc78ff86613d904a4ee8
jeff <jeff@debian83>
2017-01-10 12:08:55 +0800

    Add cv_id names.json for medical companies involved cvs

--- tests/test_model.py
+++ tests/test_model.py
@@ -24,6 +24,10 @@ kgr_file = 'tests/known_good_jd_cv_mapping.yaml'
 with open(kgr_file) as f:
     datas = yaml.load(f)
 
+med_co_cv_file = 'tmp/co_cv_names_latest.json'
+with open(med_co_cv_file) as f:
+    med_co_cv_ids = json.load(f)
+
 
 def kgr_percentage(jd_id, jd_service, sim, cvs=None, index_service=None, filterdict=None, percentage=100, above_allow=False):
     """

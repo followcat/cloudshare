c9ff16bd5f4d8440c6ceb33693fbfdf2ee0087e5
kabessC <chenjunkai1024@outlook.com>
2017-01-13 18:54:32 +0800

    Refactor Company components.

---
+++ webapp/app/src/views/ProjectManagement/component/Company.js
@@ -0,0 +1,141 @@
+'use strict';
+import React, { Component, PropTypes } from 'react';
+
+import { message } from 'antd';
+
+import { API } from '../../../config/api';
+import { confirmExcelUpload } from '../../../request/company';
+
+import StorageUtil from '../../../utils/storage';
+
+import websiteText from '../../../config/website-text';
+
+const language = websiteText.zhCN;
+
+const parseResponse = (data, info) => {
+  let idObject = {},
+      result = [];
+
+  for (let i = 0, len = data.length; i < len; i++) {
+    let item = data[i],
+        id = item[1];
+
+    if (item[0] === 'projectadd' || item[0] === 'repoadd') {
+      if (!idObject.hasOwnProperty(id)) {
+        idObject[id] = result.length;
+        result.push(Object.assign({}, info[id], {
+          type: item[0]
+        }));
+      }
+    } else {
+      if (idObject.hasOwnProperty(id)) {
+        if (typeof result[idObject[id]].diff === 'undefined') {
+          result[idObject[id]].diff = [];
+        }
+        result[idObject[id]].diff.push({
+          dataIndex: item[2][1],
+          value: item[2][2]
+        });
+      } else {
+        idObject[id] = result.length;
+        result.push(Object.assign({}, info[id], { 
+          diff: [{
+            dataIndex: item[2][1],
+            value: item[2][2]
+          }],
+          type: null
+        }));
+      }
+    }
+  }
+
+  return result;
+};
+
+class Company extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fileList: [],
+      previewList: [],
+      dataSource: [],
+      loading: false,
+      status: 'ready'
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleConfirmUpload = this.handleConfirmUpload.bind(this);
+  }
+
+  handleChange(info) {
+    const file = info.file;
+
+    if (file.status === 'done' && file.response) {
+      if (file.response.code === 200) {
+        const dataSource = parseResponse(file.response.data, file.response.info);
+        this.setState({
+          previewList: file.response.data,
+          dataSource: dataSource
+        });
+        this.props.history.push('/company/uploader');
+      }
+    }
+  }
+
+  handleConfirmUpload() {
+    this.setState({
+      loading: true,
+      status: 'doing'
+    });
+
+    confirmExcelUpload({
+      data: this.state.previewList
+    }, json => {
+      if (json.code === 200) {
+        message.success(language.UPLOAD_SUCCESS_MSG);
+        this.setState({
+          loading: false,
+          status: 'done'
+        });
+        this.props.history.push('/company/list');
+      }
+    });
+  }
+
+  render() {
+    const uploadProps = {
+      name: 'files',
+      action: API.UPLOAD_EXCEL_API,
+      header: {
+        'Authorization': `Basic ${StorageUtil.get('token')}`,
+        'Content-Type': 'application/json'
+      },
+      data: {
+        project: StorageUtil.get('_pj')
+      },
+      onChange: this.handleChange
+    };
+
+    return (
+      <div>
+        {this.props.children &&
+          React.cloneElement(this.props.children, {
+            uploadProps: uploadProps,
+            loading: this.state.loading,
+            status: this.state.status,
+            dataSource: this.state.dataSource,
+            onConfirmUpload: this.handleConfirmUpload
+          })}
+      </div>
+    );
+  }
+}
+
+Company.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.arrayOf(PropTypes.element)
+  ]),
+  history: PropTypes.object
+};
+
+export default Company;
---
+++ webapp/app/src/views/ProjectManagement/component/Customer.js
@@ -0,0 +1,229 @@
+'use strcit';
+import React, { Component } from 'react';
+
+import TablePlus from '../../../components/table-plus';
+import SiderPanel from '../../../components/sider-panel';
+import DetailInformation from '../../../components/detail-information';
+import ExtractInfo from './ExtractInfo';
+
+import { Popconfirm, message } from 'antd';
+
+import { getCustomerList, updateCustomer } from '../../../request/company';
+
+import websiteText from '../../../config/website-text';
+
+const language = websiteText.zhCN;
+
+const extractValueToString = (key, array) => {
+  return array.map(v => v[key])
+              .join(', ');
+};
+
+class OwnCustomer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dataSource: [],
+      loading: false,
+      siderPanelVisible: false,
+      detailData: {}
+    };
+    this.handleDeleteCustomerConfirm = this.handleDeleteCustomerConfirm.bind(this);
+    this.handleViewDetailsClick = this.handleViewDetailsClick.bind(this);
+    this.handleSiderPanelClose = this.handleSiderPanelClose.bind(this);
+    this.getDataSource = this.getDataSource.bind(this);
+  }
+
+  componentDidMount() {
+    this.getDataSource();
+  }
+
+  handleDeleteCustomerConfirm(id) {
+    updateCustomer('DELETE', {
+      id: id
+    }, (json) => {
+      if (json.code === 200) {
+        message.success(language.DELETE_SUCCESS_MSG);
+        this.getDataSource();
+      } else {
+        message.error(language.DELETE_FAIL_MSG);
+      }
+    });
+  }
+
+  handleViewDetailsClick(record) {
+    this.setState({
+      siderPanelVisible: true,
+      detailData: record
+    });
+  }
+
+  handleSiderPanelClose() {
+    this.setState({
+      siderPanelVisible: false
+    });
+  }
+
+  getDataSource() {
+    this.setState({
+      loading: true
+    });
+
+    getCustomerList(json => {
+      if (json.code === 200) {
+        this.setState({
+          dataSource: json.data,
+          loading: false
+        });
+      }
+    });
+  }
+
+  render() {
+    // 主体表格列数组
+    const columns = [{
+      title: language.COMPANY_NAME,
+      dataIndex: 'name',
+      key: 'name',
+      width: '14%'
+    }, {
+      title: language.DISTRICT,
+      dataIndex: 'district',
+      key: 'district',
+      width: '10%'
+    }, {
+      title: language.PRODUCT,
+      dataIndex: 'product',
+      key: 'product',
+      width: '16%'
+    }, {
+      title: language.TELLPHONE,
+      dataIndex: 'conumber',
+      key: 'conumber',
+      width: '12%'
+    }, {
+      title: language.EMAIL,
+      dataIndex: 'email',
+      key: 'email',
+      width: '12%'
+    }, {
+      title: language.OPEN_POSITION,
+      key: 'position',
+      width: '14%',
+      render: (text, record) => {
+        return <span>{extractValueToString('content', record.position)}</span>;
+      }
+    }, {
+      title: language.NEW_VISITING_SITUATION,
+      key: 'progress',
+      width: '12%',
+      render: (text, record) => {
+        return record.progress.length > 0 ?
+            <span>{record.progress[0].content}</span> :
+            null;
+      }
+    }, {
+      title: language.OPERATION,
+      key: 'operation',
+      width: '10%',
+      render: (text, record) => {
+        return (
+          <ul>
+            <li>
+              <Popconfirm
+                title={language.DELETE_CONFIRM_MSG}
+                onConfirm={() => this.handleDeleteCustomerConfirm(record.id)}
+              >
+                <a href="javascript: void(0);">
+                  {language.DELETE}
+                </a>
+              </Popconfirm>
+            </li>
+            <li>
+              <a
+                href="javascript: void(0);"
+                onClick={() => this.handleViewDetailsClick(record)}
+              >
+                {language.VIEW_DETAILS}
+              </a>
+            </li>
+          </ul>
+        );
+      }
+    }];
+
+    // 基本信息条目
+    const rows = [{
+      title: language.COMPANY_NAME,
+      dataIndex: 'name',
+      key: 'name'
+    }, {
+      title: language.PRODUCT,
+      dataIndex: 'product',
+      key: 'product'
+    }, {
+      title: language.TELLPHONE,
+      dataIndex: 'conumber',
+      key: 'conumber'
+    }, {
+      title: language.EMAIL,
+      dataIndex: 'email',
+      key: 'email'
+    }, {
+      title: language.ADDRESS,
+      dataIndex: 'address',
+      key: 'address'
+    }, {
+      title: language.WEBSITE,
+      dataIndex: 'website',
+      key: 'website',
+      render: (record) => {
+        return <a href={record} target="_blank">{record}</a>;
+      }
+    }, {
+      title: language.COMPANY_INTRODUCTION,
+      dataIndex: 'introduction',
+      key: 'introduction',
+      type: 'textarea'
+    }];
+
+    return (
+      <div>
+        <TablePlus
+          rowKey={record => record.id}
+          isToolbarShowed={true}
+          isSearched={true}
+          columns={columns}
+          expandedRowRender={record => <p>{record.introduction}</p>}
+          dataSource={this.state.dataSource}
+          loading={this.state.loading}
+        />
+        <SiderPanel
+          title={language.DETAILS_INFORMATION}
+          visible={this.state.siderPanelVisible}
+          onClose={this.handleSiderPanelClose}
+        >
+          <ExtractInfo 
+            editable={false}
+            title={language.EXTENDED_INFORMATION}
+            dataSource={this.state.detailData}
+            onCreate={this.handleExtractInfoCreate}
+            onRemove={this.handleExtractInfoRemove}
+          />
+          <DetailInformation
+            title={language.BASIC_INFORMATION}
+            rows={rows}
+            dataSource={this.state.detailData}
+            editable={false}
+            saveText={language.SAVE}
+            cancelText={language.CANCEL}
+            editText={language.EDIT}
+            onSave={this.handleBasicInfoSave}
+          />
+        </SiderPanel>
+      </div>
+    );
+  }
+}
+
+export default OwnCustomer;

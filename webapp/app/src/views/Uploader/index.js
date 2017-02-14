2e092b4b57b559409b5fc87ba0f4d9519bfefa36
kabessC <chenjunkai1024@outlook.com>
2017-02-14 17:39:43 +0800

    Finished rewrite Uploader page and their components.

---
+++ webapp/app/src/views/Uploader/index.js
@@ -0,0 +1,12 @@
+'use strict';
+import './index.less';
+
+import Layout from './component/Layout';
+import Uploader from './component/Uploader';
+import UploaderResult from './component/UploaderResult';
+
+export {
+  Layout,
+  Uploader,
+  UploaderResult
+};

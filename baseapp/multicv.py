682deacb4a62961053b20d01d1f18a5c93bf500d
followcat <followcat@163.com>
2017-03-03 10:22:10 +0800

    Add new projects.

--- baseapp/multicv.py
+++ baseapp/multicv.py
@@ -4,4 +4,5 @@ from baseapp.projects import *
 import services.multicv
 
 
-SVC_MULT_CV = services.multicv.MultiCV([SVC_PRJ_MED, SVC_PRJ_AI], SVC_CV_REPO, SVC_CLS_CV)
+SVC_MULT_CV = services.multicv.MultiCV([SVC_PRJ_MED, SVC_PRJ_AI, SVC_PRJ_BT, SVC_PRJ_IA, SVC_PRJ_NE],
+                                        SVC_CV_REPO, SVC_CLS_CV)

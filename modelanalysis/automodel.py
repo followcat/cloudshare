54c7e4515c3dc332752e2f02428c9666c167fbd9
followcat <followcat@163.com>
2017-03-15 16:53:03 +0800

    Add modify methods in automodel to class Automodels.

--- modelanalysis/automodel.py
+++ modelanalysis/automodel.py
@@ -10,136 +10,141 @@ from utils.builtin import jieba_cut, pos_extract
 import modelanalysis.judge
 
 
-def unit_gen(sources):
-    for id in sources:
-        for unit in sources[id]:
-            yield id, unit
+class Automodels(object):
+
+    FLAGS = ['s', 'x']
+
+    def __init__(self, sources, flags=None):
+        """
+            >>> from modelanalysis.automodel import *
+            >>> from baseapp.projects import *
+            >>> sources = dict(map(lambda x: (x['id'], x['description']),
+            ...                 SVC_PRJ_MED.jobdescription.lists()))
+            >>> am = Automodels(sources)
+            >>> models = am.gen_models()
+        """
+        if flags is not None:
+            self.FLAGS = flags
+        self.sources = sources
+
+    def unit_gen(self, sources):
+        for id in sources:
+            for unit in sources[id]:
+                yield id, unit
+
+    def source_list(self):
+        d = dict()
+        for id in self.sources:
+            d[id] = self.sources[id].keys()
+        return d
+
+    def source_reloaded(self):
+        d = collections.defaultdict(dict) 
+        for id in self.sources:
+            d[id] = collections.defaultdict(dict)
+            for unit in re.split(u'[\n,.。，;]', self.sources[id]):
+                d[id][unit] = {'used': set(),
+                                   'todo': set(pos_extract(jieba_cut(unit, pos=True),
+                                                           self.FLAGS))}
+        return d
+
+    def origin_unit(self, sources, used_units, todos):
+        for id in sources:
+            for inputunit in sources[id]:
+                if inputunit not in used_units:
+                    for todo in todos:
+                        if todo in inputunit:
+                            return inputunit
+
+    def gen_models(self):
+        models = []
+        sources = self.source_reloaded()
+        for id in sources:
+            for unit in sources[id]:
+                used_units = list()
+                while(sources[id][unit]['todo']):
+                    inputunit = self.origin_unit(sources, used_units, sources[id][unit]['todo'])
+                    if inputunit is None:
+                        break
+                    used_units.append(inputunit)
+                    indexs = self.train_by_model(id, inputunit, sources, models) # train_by_source
+                    if not indexs:
+                        continue
+                    for index in indexs:
+                        finished = set(models[index].dictionary.values()).intersection(
+                                       sources[id][unit]['todo'])
+                        for finish in finished:
+                            sources[id][unit]['todo'].remove(finish)
+                            sources[id][unit]['used'].add(finish)
+        return models
+
+    def origin_model(self, id, unit):
+        model = core.mining.lsimodel.LSImodel('')
+        model.slicer = services.mining.silencer
+        try:
+            model.setup([id], [model.slicer(unit)])
+        except ValueError:
+            model.names.append(id)
+            model.texts.append(model.slicer(unit))
+        return model
 
 
-def source_list(sources):
-    d = dict()
-    for id in sources:
-        d[id] = sources[id].keys()
-    return d
-
-
-def source_reloaded(sources):
-    d = collections.defaultdict(dict) 
-    for id in sources:
-        d[id] = collections.defaultdict(dict)
-        for unit in re.split(u'[\n,.。，;]', sources[id]):
-            d[id][unit] = {'used': set(),
-                               'todo': set(pos_extract(jieba_cut(unit, pos=True),
-                                                       ['s', 'x']))}
-    return d
-
-
-def origin_unit(sources, used_units, todos):
-    for id in sources:
-        for inputunit in sources[id]:
-            if inputunit not in used_units:
-                for todo in todos:
-                    if todo in inputunit:
-                        return inputunit
-
-
-def gen_models(sources):
-    """
-        >>> from modelanalysis.automodel import *
-        >>> from baseapp.projects import *
-        >>> sources = dict(map(lambda x: (x['id'], x['description']),
-        ...                 SVC_PRJ_MED.jobdescription.lists()))
-        >>> loaded_sources = source_reloaded(sources)
-        >>> models = gen_models(loaded_sources)
-    """
-    models = []
-    for id in sources:
-        for unit in sources[id]:
-            used_units = list()
-            while(sources[id][unit]['todo']):
-                inputunit = origin_unit(sources, used_units, sources[id][unit]['todo'])
-                if inputunit is None:
+    def train_by_source(self, id, requirement, sources, models, skyline=1.5):
+        des = self.source_reloaded()
+        indexs = []
+        while(des):
+            index = None
+            for i in range(len(models)):
+                sumgood = modelanalysis.judge.linalg(requirement, models[i])
+                if sumgood > skyline:
+                    index = i
+                    model = models[i]
+            else:
+                try:
+                    model = origin_model(id, requirement)
+                except ValueError:
+                    continue
+            for id, unit in unit_gen(des):
+                sumgood = modelanalysis.judge.linalg(unit, model)
+                if sumgood > skyline:
+                    model = core.mining.lsimodel.LSImodel('/tmp/lsimodel')
+                    model.slicer = services.mining.silencer
+                    try:
+                        model.setup(model.names+[id], model.texts+[model.slicer(unit)])
+                    except ValueError:
+                        continue
+                    if index is None:
+                        models.append(model)
+                        index = models.index(model)
+                    else:
+                        models[index] = model
+                    des.pop(id)
+                    indexs.append(index)
                     break
-                used_units.append(inputunit)
-                indexs = train_by_model(id, inputunit, sources, models) # train_by_source
-                if not indexs:
-                    continue
-                for index in indexs:
-                    finished = set(models[index].dictionary.values()).intersection(
-                                   sources[id][unit]['todo'])
-                    for finish in finished:
-                        sources[id][unit]['todo'].remove(finish)
-                        sources[id][unit]['used'].add(finish)
-    return models
+            else:
+                break
+        return indexs
 
 
-def origin_model(id, unit):
-    model = core.mining.lsimodel.LSImodel('')
-    model.slicer = services.mining.silencer
-    try:
-        model.setup([id], [model.slicer(unit)])
-    except ValueError:
-        model.names.append(id)
-        model.texts.append(model.slicer(unit))
-    return model
-
-
-def train_by_source(id, requirement, sources, models, skyline=1.5):
-    des = source_list(sources)
-    indexs = []
-    while(des):
+    def train_by_model(self, id, requirement, sources, models, skyline=1.5):
+        indexs = []
         index = None
-        for i in range(len(models)):
-            sumgood = modelanalysis.judge.linalg(requirement, models[i])
-            if sumgood > skyline:
-                index = i
-                model = models[i]
-        else:
-            try:
-                model = origin_model(id, requirement)
-            except ValueError:
-                continue
-        for id, unit in unit_gen(des):
-            sumgood = modelanalysis.judge.linalg(unit, model)
-            if sumgood > skyline:
-                model = core.mining.lsimodel.LSImodel('/tmp/lsimodel')
-                model.slicer = services.mining.silencer
-                try:
-                    model.setup(model.names+[id], model.texts+[model.slicer(unit)])
-                except ValueError:
-                    continue
-                if index is None:
-                    models.append(model)
-                    index = models.index(model)
-                else:
-                    models[index] = model
-                des.pop(id)
-                indexs.append(index)
+        model = self.origin_model(id, requirement)
+        resources = self.source_reloaded()
+        resources.pop(id)
+        while(resources):
+            for reid, resource in self.unit_gen(resources):
+                sumgood = modelanalysis.judge.linalg(resource, model)
+                if sumgood > skyline:
+                    try:
+                        model.setup(model.names+[reid], model.texts+[model.slicer(resource)])
+                    except ValueError:
+                        continue
+                    resources.pop(reid)
+                    break
+            else:
                 break
-        else:
-            break
-    return indexs
-
-
-def train_by_model(id, requirement, sources, models, skyline=1.5):
-    indexs = []
-    index = None
-    model = origin_model(id, requirement)
-    resources = source_list(sources)
-    resources.pop(id)
-    while(resources):
-        for reid, resource in unit_gen(resources):
-            sumgood = modelanalysis.judge.linalg(resource, model)
-            if sumgood > skyline:
-                try:
-                    model.setup(model.names+[reid], model.texts+[model.slicer(resource)])
-                except ValueError:
-                    continue
-                resources.pop(reid)
-                break
-        else:
-            break
-    if model.names:
-        models.append(model)
-        indexs.append(models.index(model))
-    return indexs
+        if model.names:
+            models.append(model)
+            indexs.append(models.index(model))
+        return indexs

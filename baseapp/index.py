0a480a6227557363f0d7830897c9282de2ebe94d
followcat <followcat@163.com>
2017-04-05 14:55:40 +0800

    Modify baseapp, add load and reload method and sync_reload RESTful API.

--- baseapp/index.py
+++ baseapp/index.py
@@ -2,5 +2,9 @@ import services.index
 from baseapp.multicv import SVC_MULT_CV
 
 
-SVC_INDEX = services.index.ReverseIndexing('Index', SVC_MULT_CV)
-SVC_INDEX.setup()
+def load_index():
+    SVC_INDEX = services.index.ReverseIndexing('Index', SVC_MULT_CV)
+    SVC_INDEX.setup()
+    return SVC_INDEX
+
+SVC_INDEX = load_index()
--- webapp/views/views.py
+++ webapp/views/views.py
@@ -16,6 +16,18 @@ import extractor.information_explorer
 import json
 
 
+class SyncReload(flask.views.MethodView):
+
+    def get(self):
+        result = False
+        ip = flask.request.remote_addr
+        if ip == '127.0.0.1':
+            reload_method = flask.current_app.config['SYNC_METHOD_RELOAD']
+            reloaded_obj = reload_method()
+            flask.current_app.config.from_object(reloaded_obj)
+            result = True
+        return str(result)
+
 class LoginRedirect(flask.views.MethodView):
 
     def get(self):

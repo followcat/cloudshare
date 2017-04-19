026e6bf584913f856372eceea5dbabb6785c866a
followcat <followcat@163.com>
2017-04-19 10:42:48 +0800

    Remove unused apscheduler.

--- run.py
+++ run.py
@@ -1,17 +1,6 @@
-import atexit
-
-from apscheduler.schedulers.background import BackgroundScheduler
-
 import webapp.server
 
 
 app = webapp.server.app
-sched = BackgroundScheduler()
-atexit.register(lambda: sched.shutdown(wait=True))
-
-
 if __name__ == '__main__':
-    #sched.add_job(app.config['SVC_MIN'].update_model, 'cron', hour='23')
-    #sched.add_job(app.config['SVC_INDEX'].update, 'cron', hour='4')
-    #sched.start()
     app.run(debug=False, host='0.0.0.0', port=4888, threaded=True)

d2b66f3beaff6f48e708ba9b64ae481199527b6b
followcat <followcat@163.com>
2017-04-28 15:36:41 +0800

    Remove unused import and var in demoapp.

--- demoapp/settings.py
+++ demoapp/settings.py
@@ -1,5 +1,4 @@
 from baseapp.datadbs import *
-from baseapp.projects import *
 from baseapp.mining import *
 from baseapp.multicv import *
 
@@ -8,6 +7,3 @@ import webapp.jsonencoder
 
 
 RESTFUL_JSON = {'cls': webapp.jsonencoder.CustomJSONEncoder}
-
-UPLOAD_TEMP = 'output'
-SECRET_KEY = 'SET T0 4NY SECRET KEY L1KE RAND0M H4SH'

350f924408d8b44e07b493e7d613cb87bb21bcf5
followcat <followcat@163.com>
2017-07-14 18:16:27 +0800

    Add baseapp index esindex loader.

--- baseapp/index.py
+++ baseapp/index.py
@@ -1,10 +1,16 @@
 import services.index
-import services.esindex
 from baseapp.multicv import SVC_MULT_CV
 
 def load_index():
-    SVC_INDEX = services.esindex.ElasticsearchIndexing(SVC_MULT_CV.svcls)
+    SVC_INDEX = services.esindex.ReverseIndexing(SVC_MULT_CV)
     SVC_INDEX.setup()
     return SVC_INDEX
 
-SVC_INDEX = load_index()
+def load_esindex():
+    import services.esindex
+    from baseapp.datadbs import SVC_CV_REPO, SVC_CV_STO
+    SVC_INDEX = services.esindex.ElasticsearchIndexing([SVC_CV_REPO, SVC_CV_STO])
+    SVC_INDEX.setup()
+    return SVC_INDEX
+
+SVC_INDEX = load_esindex()

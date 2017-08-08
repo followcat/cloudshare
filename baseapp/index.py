d0877dddb07467c016516ccd2e379b822fe57633
followcat <followcat@163.com>
2017-08-08 15:49:57 +0800

    Fix searchengine import and baseapp setting sync_reload method.

--- baseapp/index.py
+++ baseapp/index.py
@@ -4,6 +4,6 @@ import baseapp.loader
 #from baseapp.multicv import SVC_MULT_CV
 #SVC_INDEX = baseapp.loader.load_index(SVC_MULT_CV)
 
-import searchengine
+import baseapp.searchengine
 from baseapp.datadbs import SVC_CV_REPO, SVC_CV_STO
-SVC_INDEX = baseapp.loader.load_esindex(searchengine.ES, [SVC_CV_REPO, SVC_CV_STO])
+SVC_INDEX = baseapp.loader.load_esindex(baseapp.searchengine.ES, [SVC_CV_REPO, SVC_CV_STO])

fcfa51413b09daff1516beb4b089b00938f4319b
followcat <followcat@163.com>
2017-09-20 12:14:26 +0800

    Fix login manager init method.

--- webapp/ext/views.py
+++ webapp/ext/views.py
@@ -6,7 +6,7 @@ import webapp.views.account
 def init_login(app):
     login_manager = flask.ext.login.LoginManager()
     login_manager.login_view = "/"
-    login_manager.setup_app(app)
+    login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_user(id):

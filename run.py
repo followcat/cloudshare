ab87c851950b754dd90c72b6e7f692fe544a4ec7
followcat <followcat@163.com>
2017-09-26 13:12:51 +0800

    Disable webapp debug mode.

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import webapp.utils.log
 
 app = webapp.server.app
 if __name__ == '__main__':
-    app.debug = True
+    app.debug = False
     webapp.utils.log.init_smslog(app)
     if app.debug is False:
         webapp.utils.log.init_webapp_userlog(app)

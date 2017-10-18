d757966bee494af810707ffc814b7fed29235b3f
followcat <followcat@163.com>
2017-10-18 17:11:05 +0800

    Add service jobdescription class to simulationjd base class.

--- services/simulationjd.py
+++ services/simulationjd.py
@@ -1,5 +1,7 @@
+import services.jobdescription
 import services.base.simulation
 
 
-class SimulationJD(services.base.simulation.Simulation):
+class SimulationJD(services.base.simulation.Simulation,
+                   services.jobdescription.JobDescription):
     pass

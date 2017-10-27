af26fbceaddd0af6c86a668853d86f50a1a181b0
followcat <followcat@163.com>
2017-10-27 11:15:50 +0800

    Modify service cutword to support id exists '/'.

--- services/analysis/cutword.py
+++ services/analysis/cutword.py
@@ -1,6 +1,7 @@
 import os
 import ujson
 
+import utils.builtin
 import core.outputstorage
 import services.base.storage
 
@@ -10,28 +11,36 @@ class Cutword(services.base.storage.BaseStorage):
     commitinfo = 'Cutword'
 
     def unique(self, docobj):
-        return not self.exists(docobj.name)
+        return not self.exists(self.filename(docobj.name).base)
 
     def datas(self):
         for id in self.ids:
             yield id, self.getyaml(id)
 
     def exists(self, id):
-        infoname = core.outputstorage.ConvertName(id).yaml
-        return os.path.exists(os.path.join(self.path, infoname))
+        filename = self.filename(id)
+        return os.path.exists(os.path.join(self.path, filename.yaml))
 
     def getyaml(self, id):
-        name = core.outputstorage.ConvertName(id).yaml
-        yaml_str = self.interface.get(name)
+        filename = self.filename(id)
+        yaml_str = self.interface.get(filename.yaml)
         return ujson.loads(yaml_str)
 
     def add(self, docobj, committer=None, unique=True, yamlfile=True):
-        name = core.outputstorage.ConvertName(docobj.name)
+        filename = self.filename(docobj.name)
         if unique is True and self.unique(docobj) is False:
             return False
-        message = "Add %s: %s metadata." % (self.commitinfo, name)
-        self.interface.add(name.yaml, ujson.dumps(docobj.metadata,
-                           ensure_ascii=False),
-                           message=message, committer=committer)
-        self._nums += 1
-        return True
+        message = "Add %s: %s metadata." % (self.commitinfo, filename.base)
+        try:
+            self.interface.add(filename.yaml, ujson.dumps(docobj.metadata,
+                               ensure_ascii=False),
+                               message=message, committer=committer)
+            result = True
+            self._nums += 1
+        except IOError:
+            result = False
+        return result
+
+    def filename(self, name):
+        name = core.outputstorage.ConvertName(utils.builtin.industrytopath(name))
+        return name

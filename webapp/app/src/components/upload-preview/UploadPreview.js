bab131a7646db6cfc5b03d13722f2ffaf69d8dab
rootwish <rootwish233@gmail.com>
2017-12-05 18:34:50 +0800

    Add resume detail page in uploadPreview and Fix some resume detail page bug

--- webapp/app/src/components/upload-preview/UploadPreview.js
+++ webapp/app/src/components/upload-preview/UploadPreview.js
@@ -3,13 +3,15 @@ import React, { Component, PropTypes } from 'react';
 
 import PreviewWrapper from './PreviewWrapper';
 import ResumeContent from 'components/resume-content';
+import ResumeTemplate from 'components/resume-template';
 
-import { Form } from 'antd';
+import { Form, Tabs } from 'antd';
 
 class UploadPreview extends Component {
   render() {
     const {
       currentPreview,
+      dataSource,
       index,
       html,
       children
@@ -20,9 +22,14 @@ class UploadPreview extends Component {
         actived={currentPreview === index}
       >
         {children}
-        <ResumeContent
-          html={html}
-        />
+      <Tabs defaultActiveKey="chinese">
+        <Tabs.TabPane tab="中文" key="chinese">
+          <ResumeTemplate dataSource={dataSource} />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="原文" key="html">
+          <ResumeContent html={html} />
+        </Tabs.TabPane>
+      </Tabs>
       </PreviewWrapper>
     );
   }

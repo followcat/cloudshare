ea08d618598f237dd5b2ffdcbeb03612c6e392cc
followcat <followcat@163.com>
2017-12-08 10:40:17 +0800

    Upgrade svc_index in update_cloudshare with content=True.

--- tools/update_cloudshare.py
+++ tools/update_cloudshare.py
@@ -15,7 +15,7 @@ tools.updater.update_co_sims('comatch', SVC_MIN, [SVC_CV_REPO])
 tools.updater.update_pos_sims('posmatch', SVC_MIN, [SVC_CV_REPO])
 tools.updater.update_prj_sims('prjmatch', SVC_MIN, [SVC_CV_REPO])
 SVC_INDEX.updatesvc(SVC_INDEX.config['CV_STO'], SVC_CV_STO.id,
-                    SVC_CV_STO, content=False, numbers=5000)
+                    SVC_CV_STO, content=True, numbers=5000)
 ids = [each[2]['id'] for each in SVC_ADD_SYNC.yamls_gen(filterfunc=update_bydate)]
 SVC_INDEX.upgradesvc(SVC_INDEX.config['CV_STO'], SVC_CV_STO.id, SVC_CV_STO,
                      ids=ids, numbers=5000)

358b54941abd97378395558df4875c7bb9127c5c
followcat <followcat@163.com>
2017-12-20 15:38:38 +0800

    Add mining setting MIN_ADDITIONALS.

--- baseapp/mining.py
+++ baseapp/mining.py
@@ -6,7 +6,8 @@ from baseapp.member import SVC_MEMBERS
 
 
 SVC_MIN = baseapp.loader.load_mining(services.mining.silencer)
+MIN_ADDITIONALS = { u'综合': SVC_CV_STO }
 baseapp.loader.load_cv_mining(SVC_MIN, SVC_MEMBERS)
-baseapp.loader.load_addedcv_mining(SVC_MIN, SVC_MEMBERS, {'*': [u'综合']})
+baseapp.loader.load_addedcv_mining(SVC_MIN, SVC_MEMBERS, {'*': MIN_ADDITIONALS.keys()})
 baseapp.loader.load_jd_mining(SVC_MIN, [SVC_JD_REPO])
 baseapp.loader.load_co_mining(SVC_MIN, [SVC_CV_REPO])
